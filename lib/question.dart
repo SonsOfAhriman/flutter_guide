19dea8ec8556bd2a52f6ed6b67fae6c941285cfd
SonsOfAhriman <wryanhuddleston@gmail.com>
2022-06-03 13:57:15 +0900

    finished quiz app

--- lib/question.dart
+++ lib/question.dart
@@ -4,12 +4,20 @@ import 'package:flutter/src/widgets/framework.dart';
 import "package:flutter/material.dart";
 
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText;
 
   Question(this.questionText);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
